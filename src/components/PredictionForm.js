da2dac5c0e717b93efd0f703475a874b3fa80ba8
dimuthu <phdimuthukumara@gmail.com>
2024-04-08 00:56:18 +0530

    remove extra params and fine tuning

--- src/components/PredictionForm.js
+++ src/components/PredictionForm.js
@@ -7,32 +7,36 @@ function PredictionForm() {
     store_nbr: 2,
     family: "AUTOMOTIVE",
     onpromotion: "0",
-    type_x: "Holiday",
-    locale: "Local",
-    locale_name: "Manta",
-    city: "Quito",
-    state: "Pichincha",
     type_y: "B",
     cluster: "3",
     dcoilwtico: 60.0,
-    transactions: 1000,
-    dayofweek: 1,
-    dayofyear: 200,
     day: 15,
     month: 1,
-    quarter: 1,
     year: 2023,
   });
 
   const handleChange = (event) => {
+    const { name, value } = event.target;
+    // For numeric fields, parse the value as a float
+    const numericFields = ["store_nbr", "dcoilwtico", "day", "month", "year"];
+    const parsedValue = numericFields.includes(name)
+      ? parseFloat(value)
+      : value;
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
   const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Convert dcoilwtico to float before sending it
+      const formDataWithNumericDcoilwtico = {
+        ...formData,
+        dcoilwtico: parseFloat(formData.dcoilwtico),
+      };
+
       const headersList = {
         Accept: "application/json",
         "Content-Type": "application/json",
@@ -41,7 +45,7 @@ function PredictionForm() {
       const requestOptions = {
         method: "POST",
         headers: headersList,
-        body: JSON.stringify(formData),
+        body: JSON.stringify(formDataWithNumericDcoilwtico), // Send formDataWithNumericDcoilwtico
       };
 
       const response = await fetch(
@@ -51,13 +55,10 @@ function PredictionForm() {
 
       if (!response.ok) {
         const errorResponse = await response.json();
-        // setPrediction(null);
         throw new Error(errorResponse.error || "An error occurred");
       }
 
       const data = await response.json();
-
-      // Handle response data
       setPrediction(data.prediction);
     } catch (error) {
       console.error("Error fetching prediction:", error);
@@ -67,7 +68,6 @@ function PredictionForm() {
 
   return (
     <div>
-      <h2>Enter Prediction Data</h2>
       <form onSubmit={handleSubmit}>
         <div className="container">
           <div className="row">
@@ -85,7 +85,7 @@ function PredictionForm() {
             </div>
             <div className="col-md-6">
               <label className="form-label col-md-12">
-                Family:
+                Product type:
                 <select
                   className="form-control"
                   name="family"
@@ -152,147 +152,21 @@ function PredictionForm() {
             </div>
             <div className="col-md-6">
               <label className="form-label col-md-12">
-                Type X:
-                <select
-                  type="text"
+                Oil Price:
+                <input
+                  type="number"
                   className="form-control"
-                  name="type_x"
-                  value={formData.type_x}
+                  name="dcoilwtico"
+                  value={formData.dcoilwtico}
                   onChange={handleChange}
-                >
-                  <option>Holiday</option>
-                  <option>Holiday</option>
-                  <option>Transfer</option>
-                  <option>Additional</option>
-                  <option>Bridge</option>
-                  <option>Work</option> Day
-                  <option>Event</option>
-                </select>
+                />
               </label>
             </div>
           </div>
           <div className="row">
             <div className="col-md-6">
               <label className="form-label col-md-12">
-                Locale
-                <select
-                  name="locale"
-                  className="form-control"
-                  value={formData.locale}
-                  onChange={handleChange}
-                >
-                  <option>Local</option>
-                  <option>Regional</option>
-                  <option>National</option>
-                  <option>National</option>
-                  <option>Local</option>
-                </select>
-              </label>
-            </div>
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                Locale Name:
-                <select
-                  className="form-control"
-                  name="locale_name"
-                  value={formData.locale_name}
-                  onChange={handleChange}
-                >
-                  <option>Manta</option>
-                  <option>Cotopaxi</option>
-                  <option>Cuenca</option>
-                  <option>Libertad</option>
-                  <option>Riobamba</option>
-                  <option>Puyo</option>
-                  <option>Guaranda</option>
-                  <option>Imbabura</option>
-                  <option>Latacunga</option>
-                  <option>Machala</option>
-                  <option>Santo Domingo</option>
-                  <option>El Carmen</option>
-                  <option>Cayambe</option>
-                  <option>Esmeraldas</option>
-                  <option>Ecuador</option>
-                  <option>Ambato</option>
-                  <option>Ibarra</option>
-                  <option>Quevedo</option>
-                  <option>Ecuador</option>
-                  <option>Santo Domingo de los Tsachilas</option>
-                  <option>Santa Elena</option>
-                  <option>Quito</option>
-                  <option>Loja</option>
-                  <option>Salinas</option>
-                  <option>Guayaquil</option>
-                  <option>Guayaquil</option>
-                  <option>Cuenca</option>
-                  <option>Ibarra</option>
-                  <option>Quito</option>
-                </select>
-              </label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                City:
-                <select
-                  className="form-control"
-                  name="city"
-                  value={formData.city}
-                  onChange={handleChange}
-                >
-                  <option>Quito</option>
-                  <option>Santo Domingo</option>
-                  <option>Latacunga</option>
-                  <option>Riobamba</option>
-                  <option>Ibarra</option>
-                  <option>Guaranda</option>
-                  <option>Puyo</option>
-                  <option>Ambato</option>
-                  <option>Guayaquil</option>
-                  <option>Salinas</option>
-                  <option>Babahoyo</option>
-                  <option>Cuenca</option>
-                  <option>Loja</option>
-                  <option>Machala</option>
-                  <option>Esmeraldas</option>
-                  <option>Manta</option>
-                </select>
-              </label>
-            </div>
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                State:
-                <select
-                  className="form-control"
-                  name="state"
-                  value={formData.state}
-                  onChange={handleChange}
-                >
-                  <option>Pichincha</option>
-                  <option>Santo Domingo de los Tsachilas</option>
-                  <option>Cotopaxi</option>
-                  <option>Chimborazo</option>
-                  <option>Imbabura</option>
-                  <option>Bolivar</option>
-                  <option>Pastaza</option>
-                  <option>Tungurahua</option>
-                  <option>Guayas</option>
-                  <option>Santa Elena</option>
-                  <option>Los Rios</option>
-                  <option>Azuay</option>
-                  <option>Loja</option>
-                  <option>ElOro</option>
-                  <option>Esmeraldas</option>
-                  <option>Manabi</option>
-                </select>
-              </label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                Type Y:
+                Store type:
                 <select
                   className="form-control"
                   name="type_y"
@@ -320,58 +194,7 @@ function PredictionForm() {
               </label>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                dcoilwtico:
-                <input
-                  type="number"
-                  className="form-control"
-                  name="dcoilwtico"
-                  value={formData.dcoilwtico}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                Transactions:
-                <input
-                  type="number"
-                  className="form-control"
-                  name="transactions"
-                  value={formData.transactions}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                Day of Week:
-                <input
-                  type="number"
-                  className="form-control"
-                  name="dayofweek"
-                  value={formData.dayofweek}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
-                Day of Year:
-                <input
-                  type="number"
-                  className="form-control"
-                  name="dayofyear"
-                  value={formData.dayofyear}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-          </div>
+
           <div className="row">
             <div className="col-md-6">
               <label className="form-label col-md-12">
@@ -402,18 +225,6 @@ function PredictionForm() {
           <div className="row">
             <div className="col-md-6">
               <label className="form-label col-md-12">
-                Quarter:
-                <input
-                  type="number"
-                  className="form-control"
-                  name="quarter"
-                  value={formData.quarter}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <div className="col-md-6">
-              <label className="form-label col-md-12">
                 Year:
                 <input
                   type="number"
@@ -424,20 +235,32 @@ function PredictionForm() {
                 />
               </label>
             </div>
+
+            <div className="col-md-6">
+              {/* <label className="form-label col-md-12">&nbsp;</label> */}
+              <br></br>
+              <button type="submit" className="btn btn-primary">
+                {" "}
+                Submit{" "}
+              </button>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-12 prediction-result">
+              {/* {prediction && ( */}
+              <div>
+                {error && <div className="alert alert-danger">{error}</div>}
+                Sales Prediction:{" "}
+                {prediction && (
+                  <div className="prediction-text"> {prediction}</div>
+                )}
+              </div>
+              {/* )} */}
+            </div>
           </div>
         </div>
-        <br />
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
       </form>
-
-      {prediction && (
-        <div>
-          {error && <div className="alert alert-danger">{error}</div>}
-          {prediction && <div>Prediction: {prediction}</div>}
-        </div>
-      )}
     </div>
   );
 }
